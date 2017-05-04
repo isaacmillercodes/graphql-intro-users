be59b779cca02f8d54cb5ed3414492b26bfad540
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-05-04 10:36:08 -0600

    added graphql and schema file

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}));
 
 const port = process.env.PORT || 4000;
 
